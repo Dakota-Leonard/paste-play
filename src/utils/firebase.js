0e6383724d3a10b2a9a37bd50111cc49234effd2
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-30 18:35:46 -0400

    Add firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,42 @@
+// Importing SDKs
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore/lite';
+var lzma = require('lzma-native');
+
+console.log(lzma.compress('test'));
+//App configs
+const firebaseConfig = {
+  apiKey: 'AIzaSyBvvuuPmbtP1H1KcEhvxXe28H0OSVUt41o',
+  authDomain: 'paste-play-project.firebaseapp.com',
+  projectId: 'paste-play-project',
+  storageBucket: 'paste-play-project.appspot.com',
+  messagingSenderId: '263411079165',
+  appId: '1:263411079165:web:a3c29a1c157a8fa8d9eb94',
+  measurementId: 'G-PN3T67EV0K',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Initialize DB
+export const db = getFirestore(app);
+
+const printDocs = async () => {
+  const q = query(collection(db, 'logs'), where('hidden', '==', false));
+  const docs = await getDocs(q);
+  docs.forEach(doc => console.log(doc.data()));
+};
+
+// console.log(lzma.compress('test'));
+
+printDocs();
+
+export default app;
