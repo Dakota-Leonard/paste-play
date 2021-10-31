e90d126ee22ea35073569c507a70a6f1bd72469b
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-31 19:25:06 -0400

    Add associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,13 @@
 const db = require('./database');
 const Log = require('./log');
+const User = require('./user');
+
+//Associations
+User.hasMany(Log);
+Log.belongsTo(User);
 
 module.exports = {
   db,
+  User,
   Log,
 };
