a0dad18816d566002010911288127e16998215ad
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-31 13:22:22 -0400

    Got /api route proxy on 3001 to work with create-react-app on 3000

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import Button from '@mui/material/Button';
 import NavBar from './NavBar';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [routeResponse, setRouteResponse] = useState('');
+
+  useEffect(() => {
+    apiRouteTest();
+  }, []);
+
+  //Route Test
+  const apiRouteTest = async () => {
+    try {
+      const { data } = await axios.get('/api');
+      setRouteResponse(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <NavBar />
+      <h1>{routeResponse}</h1>
     </>
   );
 }
