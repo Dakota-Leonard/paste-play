b8d58819c84df41fb7d186e0e0cf180e9a71e7fa
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-11-03 20:30:18 -0400

    Removed unused imports

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { db, User, Log } = require('./db');
+const { db, User } = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
