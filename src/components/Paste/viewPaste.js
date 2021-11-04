97f22505a7d9ff8f92a014256bc930fb1d85d83e
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-11-03 21:47:07 -0400

    Format Card for displaying log

--- src/components/Paste/viewPaste.js
+++ src/components/Paste/viewPaste.js
@@ -1,9 +1,9 @@
-import { Paper } from '@mui/material';
-import React, { useEffect } from 'react';
+import { Card } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const ViewPaste = props => {
-  //Component did mount get log
+  //Component did mount so get log
   useEffect(() => {
     const fetchLog = async () => {
       const { data } = await axios.get(`/api/view/${props.match.params.url}`);
@@ -11,7 +11,28 @@ const ViewPaste = props => {
     };
     fetchLog();
   }, [props.match.params.url]);
-  return <Paper>TEST</Paper>;
+
+  const createMarkup = () => {
+    return { __html: 'First &middot; Second' };
+  };
+
+  return (
+    <Card
+      variant="elevation"
+      elevation={0}
+      sx={{
+        backgroundColor: '#000',
+        display: 'flex',
+        flexWrap: 'wrap',
+        padding: '1%',
+        alignItems: 'center',
+        width: '80%',
+        minHeight: '90vh',
+        marginLeft: '10%',
+        marginTop: '1%',
+      }}
+    ></Card>
+  );
 };
 
 export default ViewPaste;
